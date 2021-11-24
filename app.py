26ca2b22b7ce1aa598448650fd6a14cc9d52c99d
Sertalp B. Cay <sertalpbilal@gmail.com>
2021-11-23 22:04:34 -0500

    Convert messages to embed

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ league_list = ['Champions League', 'Europa League', 'Premier League']
 debug = False
 
 bot = commands.Bot(command_prefix='!')
+game_data = {}
 
 async def get_snapshot():
     date = time.strftime("%Y%m%d")
@@ -64,6 +65,8 @@ def parse_status_data(snapshot, date):
     return data
 
 async def main():
+    global game_data
+    # print(game_data)
     live_data = {}
     while True:
         date = time.strftime("%Y%m%d")
@@ -86,17 +89,66 @@ async def main():
             if event['event'] != 'liveTime':
                 print('*** PUSH TO SERVER! ', str(event))
                 channel = bot.get_channel(912383883935375440) # Bot test channel
-                await channel.send(update_text)
+                embed = discord.Embed(title=f"{match['home_name']} - {match['away_name']}", description="", color=discord.Color.green())
+                embed.add_field(name="Event", value=event['event'], inline=True)
+                embed.add_field(name="Min", value=match['liveTime'], inline=True)
+                embed.add_field(name="Score", value=f"{match['home_score']} - {match['away_score']}", inline=True)
+                embed.add_field(name="Link", value=f"https://www.fotmob.com/match/{match['match_id']}", inline=True)
+                
+                # await message.channel.send('\n'.join(bot_message))
+                await channel.send(embed=embed)
+                # await channel.send(update_text)
             print(update_text)
         else:
             print("* None")
         if debug:
             print("LIVE DATA")
             print(live_data)
+        game_data = live_data
+        # print(game_data)
         await asyncio.sleep(30)
 
 
-client = discord.Client()
+# client = discord.Client()
+
+@bot.event
+async def on_ready():
+    print(f'Live Rank Bot is working! {bot.user}')
+
+@bot.event
+async def on_message(message):
+
+    print(message)
+
+    global game_data
+
+    if message.author == bot.user:
+        return
+    if message.guild.name != 'FPL Analytics Community':
+        return
+
+    allowed_channels = [912383883935375440, 912375274430169109, 912790411951624263] # test/bot/match-chat channels
+
+    if message.channel.id not in allowed_channels:
+        return
+
+    if message.content.lower().startswith('!live'):
+        words = message.content.split()
+
+        if len(words) == 1 or words[1] == 'list':
+            # list ongoing games here
+            embed = discord.Embed(title=f"Live Games", description="", color=discord.Color.red())
+            bot_message = []
+            for match in game_data.get('matches', {}).values():
+                if not match['started'] or match['finished']:
+                    continue
+                match_text = f"Min {match['liveTime']} | {match['home_name']} - {match['away_name']}: {match['home_score']} - {match['away_score']}\n`Link: https://www.fotmob.com/match/{match['match_id']}/`" # [Started: {match['started']}, Finished: {match['finished']}]
+                bot_message.append(match_text)
+                embed.add_field(name=f"{match['home_name']} - {match['away_name']}", value=f"**Min** {match['liveTime']}\n**Score** {match['home_score']} - {match['away_score']}\n**Link** https://www.fotmob.com/match/{match['match_id']}", inline=False)
+            
+            # await message.channel.send('\n'.join(bot_message))
+            await message.channel.send(embed=embed)
+
 
 if __name__ == "__main__":
     if debug:
@@ -113,3 +165,4 @@ if __name__ == "__main__":
             config = {}
         bot.loop.create_task(main())
         bot.run(config.get('TOKEN', os.environ.get('TOKEN')))
+
